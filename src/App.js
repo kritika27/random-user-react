dbc450d868876a25c5e2d8a1206b8327e29efe9b
kritika <kritikasri27@gmail.com>
2021-03-23 19:13:29 +0530

    Added main html file and logic of generating random user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from "react"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const url="https://randomuser.me/api/"
+
+const App=()=>{
+  const [profile,setProfile]= useState([]);
+
+const fetchUser=async()=>{
+  const res=await fetch(url);
+  const data=await res.json();
+  const profile = data.results[0];
+  const {cell,email,dob:{date,age},name:{first,last},picture:{large},location:{city,country}} = profile;
+  
+const newPerson={
+  first,
+  last,
+  email,
+  cell,
+  date,
+  age,
+  large,
+  city,
+  country
+}
+setProfile(newPerson);
 }
 
-export default App;
+const newUser=()=>{
+  fetchUser();
+}
+
+useEffect(()=>{
+fetchUser();
+},[]) 
+
+  return(
+  <main>
+    <div className='block bcg-black'></div>
+      <div className='block'>
+        <div className="container">
+          <img src={profile.large} alt={profile.first}/>
+          <p className='user-value'>{profile.first} {profile.last}</p>
+          <p className='user-title'>{profile.age}</p>
+          <p className='user-title'>{profile.email}</p>   
+          <p className='user-title'>{profile.city},{profile.country}</p>
+          <button className="btn" onClick={newUser}>Surprise Me</button>
+        </div>
+      </div>
+  </main>
+    )
+    }
+export default App
